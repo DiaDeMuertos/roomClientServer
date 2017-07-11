7c49c62e9972526f2db7c417cde3a62c1edc9105
DiaDeMuertos <imworkingnow@msn.com>
2017-07-10 19:57:43 -0700

    updated client file

--- client.js
+++ client.js
@@ -1,17 +1,22 @@
 'use strict';
 
-var io = require('socket.io-client');
-var exec = require('child_process').exec;
+const io = require('socket.io-client');
+const exec = require('child_process').exec;
 
 const socket = io.connect('http://localhost:3000');
 
-socket.on('chat message', function (msg) {
-    console.log(msg);
+socket.on('video', (video) => {
+  const pathVideo = '/home/diademuertos/Desktop/video';
 
-    exec('ls', function callback(error, stdout, stderr) {
-        console.log(stdout);
-    });
+  const cmd = `vlc --play-and-exit ${pathVideo}/${video}.mp3`;
 
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(stderr);
+    } else {
+      console.log(stdout);
+    }
+  });
 });
 
-socket.emit('chat message', 'jello with j');
+socket.emit('video', 'AlienWare_1');
